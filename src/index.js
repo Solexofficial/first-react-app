ba1b1aadf019117e251dd669ec2e492ea42e03c4
Mark Pokrovsky <solexofficial@yandex.ru>
2021-08-23 21:56:50 +0300

    feat: homework-2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const element = <h1>Hello World</h1>;
+const element = <h1>Hello World меня зовут: Mark Pokrovsky</h1>;
 
 console.log(element);
 ReactDom.render(element, document.getElementById("root"));
